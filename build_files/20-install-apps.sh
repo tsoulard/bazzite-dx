ab17736ffb033e43beadd3312d3db7fe270b0e20
Tom Soulard <tsoulard@users.noreply.github.com>
2026-04-29 14:17:21 +0100

    removed deck image fixes and update iso.toml oh and install

--- build_files/20-install-apps.sh
+++ build_files/20-install-apps.sh
@@ -37,35 +37,13 @@ dnf5 --setopt=install_weak_deps=False install -y \
     edk2-ovmf \
     guestfs-tools
 
+# install extra apps
+dnf5 install -y jstest-gtk
+
 # Restore UUPD update timer and Input Remapper
 sed -i 's@^NoDisplay=true@NoDisplay=false@' /usr/share/applications/input-remapper-gtk.desktop
 systemctl enable input-remapper.service
 systemctl enable uupd.timer
-
-# Remove -deck specific changes to allow for login screens and session selection in settings
-rm -f /etc/sddm.conf.d/steamos.conf
-rm -f /etc/sddm.conf.d/virtualkbd.conf
-rm -f /etc/sddm.conf.d/zz-steamos-autologin.conf
-rm -f /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz
-systemctl disable bazzite-autologin.service
-dnf5 remove -y steamos-manager
-
-if [[ "$IMAGE_NAME" == *gnome* ]]; then
-    # Remove SDDM and re-enable GDM on GNOME builds.
-    dnf5 remove -y \
-        sddm
-
-    systemctl enable gdm.service
-else
-    # Re-enable logout and switch user functionality in KDE
-    sed -i -E \
-      -e 's/^(action\/switch_user)=false/\1=true/' \
-      -e 's/^(action\/start_new_session)=false/\1=true/' \
-      -e 's/^(action\/lock_screen)=false/\1=true/' \
-      -e 's/^(kcm_sddm\.desktop)=false/\1=true/' \
-      -e 's/^(kcm_plymouth\.desktop)=false/\1=true/' \
-      /etc/xdg/kdeglobals
-fi
 
 
 dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:ublue-os:packages" -y \
