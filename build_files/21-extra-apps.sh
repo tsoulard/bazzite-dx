c96cdb46102dd3ce2c28545129dacdacd4b13b02
Thomas Soulard <tsoulard@users.noreply.github.com>
2026-05-07 14:31:25 +0100

    Fixed copr repo setup for sunshine

--- build_files/21-extra-apps.sh
+++ build_files/21-extra-apps.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 # New Repos
-dnf5 copr enable lizardbyte/beta
+dnf5 copr enable -y lizardbyte/beta
 
 # Install extra apps
 dnf5 install -y jstest-gtk
 
-dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:lizardbyte:beta" -y \
-    Sunshine
+dnf5 install -y Sunshine
 
 # Remove Packages
 dnf5 -y remove lutris
+
+# Disable Repos
+dnf5 copr disable -y lizardbyte/beta
