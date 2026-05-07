1e84ef57392b0ac0d7d837d4bb195f5f8cf0f312
Tom Soulard <tsoulard@users.noreply.github.com>
2026-05-07 13:37:21 +0100

    Add sunshine back to the install

--- build_files/21-extra-apps.sh
+++ build_files/21-extra-apps.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# New Repos
+dnf5 copr enable lizardbyte/beta
+
 # Install extra apps
 dnf5 install -y jstest-gtk
+
+dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:lizardbyte:beta" -y \
+    Sunshine
 
 # Remove Packages
 dnf5 -y remove lutris
